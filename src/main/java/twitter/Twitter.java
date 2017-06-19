b7cca457ea1603bf9ef0c85ce695218df1378017
Gray <n0220007@lm.lmig.com>
2017-06-19 10:27:05 -0400

    added new tweet functionality

--- src/main/java/twitter/Twitter.java
+++ src/main/java/twitter/Twitter.java
@@ -46,13 +46,63 @@ public class Twitter {
 
 		});
 
+		post("/newTweet", (req, res) -> {
+			System.out.println("/newTweet");
+			String usrnm = req.queryParams("returnUserName");
+
+			//long userId = req.session().attribute("userid");
+
+			String content = req.queryParams("newTweetContent");
+			final String INSERT_TWEET = "insert into tweets (content) values (?);";
+			try (Connection conn3 = DriverManager.getConnection(url);
+					PreparedStatement statement = conn3.prepareStatement(INSERT_TWEET,
+							Statement.RETURN_GENERATED_KEYS);) {
+
+				statement.setString(1, content);
+
+				int affectedRows = statement.executeUpdate();
+
+				if (affectedRows == 0) {
+					throw new SQLException("Creating user failed, no rows affected.");
+				}
+
+				try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
+					if (generatedKeys.next()) {
+						long tweetId = generatedKeys.getLong(1);
+
+						final String INSERT_USERTWEET = "insert into userTweets (tweetId, userId) values (?, ?);";
+						try (PreparedStatement usertweetStmt = conn3.prepareStatement(INSERT_USERTWEET,
+								Statement.RETURN_GENERATED_KEYS);) {
+							usertweetStmt.setLong(1, tweetId);
+							usertweetStmt.setLong(2, 18); // TODO hardcoded as
+															// david
+							affectedRows = usertweetStmt.executeUpdate();
+							if (affectedRows == 0) {
+								throw new SQLException("Creating user failed, no rows affected.");
+							}
+						} catch (SQLException e) {
+
+						}
+
+					} else {
+						throw new SQLException("Creating user failed, no ID obtained.");
+					}
+				}
+			} catch (SQLException e) {
+				System.out.println(e.getMessage());
+			}
+			System.out.println(content);
+
+			return usrnm + content;
+		});
+
 		post("/newUser", (req, res) -> {
 			System.out.print("/newUser ");
 			String usrnm = req.queryParams("userName");
 			String passw = req.queryParams("password");
 			String email = req.queryParams("email");
-			String hashedPassw =Twitter.hashPassword(passw);
-			
+			String hashedPassw = Twitter.hashPassword(passw);
+
 			Connection conn2 = null;
 			try {
 				conn2 = DriverManager.getConnection(url);
@@ -76,21 +126,22 @@ public class Twitter {
 				}
 			}
 			System.out.println(hashedPassw);
-			
-			return usrnm; //TODO what do we want to do after we create the account? Make them login?
+
+			return usrnm; // TODO what do we want to do after we create the
+							// account? Make them login?
 		});
-		
+
 		post("/returnUser", (request, response) -> {
 			// helper code
 			System.out.print("/returnUser ");
 			System.out.println(request.queryParams());
 			String usrnm = request.queryParams("returnUserName");
 			String passw = request.queryParams("returnUserPassword");
-			String storedPassw="";
+			String storedPassw = "";
 			// real code starts here
 
 			try (Connection conn2 = DriverManager.getConnection(url)) {
-//				change the line below to select user id AND password
+				// change the line below to select user id AND password
 				String checkUserSQL = "select password from Users where userName = ?";
 				PreparedStatement checkUserStmt = null;
 				checkUserStmt = conn2.prepareStatement(checkUserSQL);
@@ -99,23 +150,22 @@ public class Twitter {
 				ResultSet rs = checkUserStmt.executeQuery();
 				{
 					while (rs.next()) {
-						System.out.println(rs.getString("password"));	
-						storedPassw =(rs.getString("password"));
+						System.out.println(rs.getString("password"));
+						storedPassw = (rs.getString("password"));
 					}
-					String hashedPassw =Twitter.hashPassword(passw);
-					System.out.println(hashedPassw);	
-					if (!storedPassw.equals(hashedPassw)){
+					String hashedPassw = Twitter.hashPassword(passw);
+					System.out.println(hashedPassw);
+					if (!storedPassw.equals(hashedPassw)) {
 						System.out.println("Invalid User, password combination");
 						return "Invalid User, password combination";
-					}
-					else {
-						System.out.println("Welcome "+ usrnm);
+					} else {
+						System.out.println("Welcome " + usrnm);
 						return "Welcome " + usrnm;
 					}
 				}
 			} catch (SQLException e) {
 				System.out.println(e.getMessage());
-			} 
+			}
 			return usrnm + passw;// what do we want to do after they login?
 
 		});
@@ -125,12 +175,12 @@ public class Twitter {
 		passw += SALT;
 
 		MessageDigest md = MessageDigest.getInstance("SHA");
-		
+
 		md.update(passw.getBytes());
 		String digest = new String(md.digest());
 		return digest;
 	}
-	
+
 	public static Connection connect() {
 		Connection conn = null;
 		try {
