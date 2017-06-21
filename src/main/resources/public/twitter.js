bfdead1d8eb64854573a22e61a53bc09a1485659
Gray <n0220007@lm.lmig.com>
2017-06-21 16:56:36 -0400

    add active user functionality, new user login

--- src/main/resources/public/twitter.js
+++ src/main/resources/public/twitter.js
@@ -74,13 +74,62 @@ newTweetButton.onclick = function() {
 // see list of popular tweets
 var popularButton = document.getElementById('popular');
 popularButton.onclick = function(evt) {
-  var xhrReturn = new XMLHttpRequest();
-  xhrReturn.open('GET', '/popular');
-  xhrReturn.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  if (xhrNewTweet.status === 200) {
-    getPopular();
-  } else if (xhrNewTweet.status !== 200) {
-    alert('Request failed.  Returned status of ' + xhrNewTweet.status);
-  }
-  xhrReturn.send();
+  var xhrPopular = new XMLHttpRequest();
+  xhrPopular.open('GET', '/popular');
+  xhrPopular.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  xhrPopular.onload = function() {
+      if (xhrPopular.status === 200) {
+          getPopular();
+      } else if (xhrPopular.status !== 200) {
+          alert('Request failed.  Returned status of ' + xhrPopular.status);
+      }
+    };
+  xhrPopular.send();
 };
+
+// see users own feed of tweets on button click
+var feedButton = document.getElementById('feed');
+feedButton.onclick = function(evt) {
+  var xhrFeed = new XMLHttpRequest();
+  xhrFeed.open('GET', '/api/feed');
+  xhrFeed.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  xhrFeed.onload = function() {
+      if (xhrFeed.status === 200) {
+          getFeed();
+      } else if (xhrFeed.status !== 200) {
+          alert('Request failed.  Returned status of ' + xhrFeed.status);
+      }
+    };
+  xhrFeed.send();
+};
+
+//get users own tweets, called by feed button click
+function getFeed() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', '/api/feed');
+  xhr.onload = function(evt) {
+    if (xhr.status === 200) {
+      var response = JSON.parse(xhr.responseText);
+      var timelineDiv = document.getElementById('timeline');
+      timelineDiv.innerHTML = '';
+      for (var i in response) {
+        var tweet = response[i];
+        var div = document.createElement('div');
+        div.setAttribute('class', 'Sizzling');
+        var ul = document.createElement('ul');
+        var username = document.createElement('li');
+        var content = document.createElement('li');
+        var tweetDt = document.createElement('li');
+        username.innerHTML = tweet.username;
+        content.innerHTML = tweet.content;
+        tweetDt.innerHTML = tweet.tweetDt;
+        ul.appendChild(username);
+        ul.appendChild(content);
+        ul.appendChild(tweetDt);
+        div.appendChild(ul);
+        timelineDiv.appendChild(div);
+      }
+    }
+  }
+  xhr.send();
+}
